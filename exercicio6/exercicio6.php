e2dadbb10bcf7da9f4f29caebbdfeb3bb2d90ed5
Leonardo Bueno Martins <leo01012005@gmail.com>
2023-11-03 22:29:12 -0300

    exercicio6

---
+++ exercicio6/exercicio6.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-b">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AAAA</title>
+</head>
+<body>
+    <?php 
+        $Narrador = 5;
+        switch($Narrador) {
+            case 5: echo"Infantil A"; break;
+            case 6: echo"Infaltil A"; break;
+            case 7: echo"Infaltil A"; break;
+            case 8: echo"Infaltil B"; break;
+            case 9: echo"Infaltil B"; break;
+            case 10: echo"Infaltil B"; break;
+            case 11: echo"Juvenil A"; break;
+            case 12: echo"Juvenil A"; break;
+            case 13: echo"Juvenil A"; break;
+            case 14: echo"Juvenil B"; break;
+            case 15: echo"Juvenil B"; break;
+            case 16: echo"Juvenil B"; break;
+            case 17: echo"Juvenil B"; break;
+            default: echo "Adulto"; break;
+        } ?>
+    </div>
+</body>
+</html>
